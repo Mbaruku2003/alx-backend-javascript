f64aa6388501811448ee0a0c3838480a206f180e
Mbaruku2003 <leonardjembe21@gmail.com>
2024-10-08 21:38:33 +0300

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -3,7 +3,7 @@ const CleanSet = (set, startString) => {
   if (startString === '' || typeof startString !== 'string') return '';
   set.forEach(s) => {
     if (typeof s === 'string' && s.startsWith(startString)) {
-      strings.push(s.slice(startStrung.length));
+      strings.push(s.slice(startString.length));
     }
   });
   return strings.join('-');

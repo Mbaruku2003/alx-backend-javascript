a9f31f5fda02a9609e7dea5b251f1849b13079f1
Mbaruku2003 <leonardjembe21@gmail.com>
2024-10-08 21:34:09 +0300

    9-groceries_list.js

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const theset = new Map();
+  theset.set('Apples', 10);
+  theset.set('Tomatoes', 10);
+  theset.set('Pasta', 1);
+  theset.set('Rice', 1);
+  theset.set('Banana', 5);
+  return theset;
+}

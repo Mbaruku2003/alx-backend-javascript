de2d23b8f1aa6382b28a41627840b5f8dd221e25
Mbaruku2003 <leonardjembe21@gmail.com>
2024-11-26 11:12:24 +0300

    created a new file

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,17 +1,17 @@
 const fs = require('fs');
 
-function countStudents(path) {
+const countStudents = (path) => {
   try {
     const data = fs.readFileSync(path, 'utf-8');
-    const lines = data.trim().split('\n').filter(line => line);
-    if (lines.length === 0) {
+    const lines = data.trim().split('\n').filter((line) => line);
+    if (lines.length <= 0) {
       throw new Error('Cannot load the database');
     }
 
     const [header, ...rows] = lines;
-    const students = rows.map(row => {
+    const students = rows.map((row) => {
       const [firstname, lastname, age, field] = row.split(',');
-      return { firstname, lastname, age, field };
+      return {firstname, lastname, age, field};
     });
     const fields = {};
     for (const student of students) {
@@ -22,10 +22,10 @@ function countStudents(path) {
     }
     console.log(`Number of students: ${students.length}`);
     for (const [field, firstnames] of Object.entries(fields)) {
-      console.log(`Number of students in ${field}: ${firstnames.length}. List ${firstnames.join(', ')}`);
+      console.log(`Number of students in ${field}: ${firstnames.length}. List: ${firstnames.join(', ')}`);
     }
   } catch (err) {
-      throw new Error('Cannot load the database');
+    throw new Error('Cannot load the database');
   }
 }
 module.exports = countStudents;

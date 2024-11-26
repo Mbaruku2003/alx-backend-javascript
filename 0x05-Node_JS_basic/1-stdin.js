8d2557ca9c43f29470860d12755d24d624a8898d
Mbaruku2003 <leonardjembe21@gmail.com>
2024-11-26 10:22:26 +0300

    created a new file

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,10 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
 process.stdin.on('data', (data) => {
   const thedata = data.toString().trim();
   process.stdout.write(`Your name is: ${thedata}\n`);
   process.exit();
 });
 process.on('exit', () => {
-  console.log('This important software is now closing');
+  console.log('This important software is now closing\n');
 });

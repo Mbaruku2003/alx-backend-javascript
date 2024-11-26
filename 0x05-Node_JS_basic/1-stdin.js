1f55c6d5b23420aca2878c31d2942b7711abdd41
Mbaruku2003 <leonardjembe21@gmail.com>
2024-11-26 09:30:41 +0300

    created a new file

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,6 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (input) => {
-  const name = input.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+  process.stdout.write(`Your name is: ${input}\n`);
   process.exit();
 });
 process.on('exit', () => {

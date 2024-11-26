efce147691dc7716fd5375dd3ad8871c1777651c
Mbaruku2003 <leonardjembe21@gmail.com>
2024-11-26 15:54:02 +0300

    created a new file

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -6,7 +6,7 @@ const http = require('http');
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!')
+  res.end('Hello Holberton School!');
 });
 app.listen(1245, () => {
   console.log('Server is running on port 1245');

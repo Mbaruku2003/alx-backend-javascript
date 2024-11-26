caf11f2d4b3dbcc6604509d25b2fac07130140a8
Mbaruku2003 <leonardjembe21@gmail.com>
2024-11-26 11:29:29 +0300

    created a new file

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,16 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+const readline = require('readline');
 
-process.stdin.on('data', (data) => {
-  const input = data.toString().trim();
-  process.stdout.write(`Your name is: ${input}\n`);
-  process.exit();
-});
-process.on('exit', () => {
-  console.log('This important software is now closing');
-});
+const promptUserName = () => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
+    console.log(`Your name is: ${name}`);
+    rl.close();
+  });
+  rl.on('close', () => {
+    console.log('This important software is now closing');
+  });
+};
+module.exports = promptUserName;

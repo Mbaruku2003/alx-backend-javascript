c7580c06e7519f37c4e5b7778816c1f31bcdf1ef
Mbaruku2003 <leonardjembe21@gmail.com>
2024-11-27 19:54:17 +0300

    created a new file

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul.js');
+
+describe('calculateNumber', () => {
+  it('should return 4 for 1 and 3', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+  it('should return 5 for 1 and 3.7', () => {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+  it('returns 5 for 1.2 and 3.7', () => {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+  it('givs ut 6 for 1.5 and 3.7', () => {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+});
---
+++ 0x06-unittests_in_js/README.md
@@ -0,0 +1,1 @@
+ll deal wit javascript testing using mocha, chai and sinon

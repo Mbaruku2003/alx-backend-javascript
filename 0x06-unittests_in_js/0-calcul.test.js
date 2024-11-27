f961a4228446908aa8e9714c8d8a28d3594166e4
Mbaruku2003 <leonardjembe21@gmail.com>
2024-11-27 21:12:47 +0300

    created a new file

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,5 +1,4 @@
 const assert = require('assert');
-const { it, describe } = require("mocha");
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
@@ -9,10 +8,10 @@ describe('calculateNumber', () => {
   it('should return 5 for 1 and 3.7', () => {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
-  it('returns 5 for 1.2 and 3.7', () => {
+  it('return 5 for 1.2 and 3.7', () => {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
-  it('givs ut 6 for 1.5 and 3.7', () => {
+  it('return 6 for 1.5 and 3.7', () => {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 });

e8c19bfeab1e9d69f816de425dc3a69028f3d1ed
Mbaruku2003 <leonardjembe21@gmail.com>
2024-11-28 15:52:32 +0300

    created a new file

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -2,6 +2,6 @@ const Utils = require('./utils');
 
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
   const amount = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is : ${amount}`);
+  console.log(`The total is: ${amount}`);
 };
 module.exports = sendPaymentRequestToApi;
--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -4,17 +4,25 @@ const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
 
 describe('SendPaymentRequestToApi', () => {
-  let spy;
+  let spyCalculateNumber;
+  let spyConsoleLog;
 
   beforeEach(() => {
-    spy = sinon.spy(Utils, 'calculateNumber');
+    spyCalculateNumber = sinon.spy(Utils, 'calculateNumber');
+    spyConsoleLog = sinon.spy(console, 'log');
   });
   afterEach(() => {
-    spy.restore();
+    spyCalculateNumber.restore();
+    spyConsoleLog.restore();
   });
   it('should call Utils.calculateNumber with SUM, 100 and 20', () => {
     sendPaymentRequestToApi(100, 20);
-    expect(spy.calledOnce).to.be.true;
-    expect(spy.calledWith('SUM', 100, 20)).to.be.true;
+    expect(spyCalculateNumber.calledOnce).to.be.true;
+    expect(spyCalculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+  });
+  it('should log the correct message to the console', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(spyConsoleLog.calledOnce).to.be.true;
+    expect(spyConsoleLog.calledWith('The total is: 120')).to.be.true;
   });
 });

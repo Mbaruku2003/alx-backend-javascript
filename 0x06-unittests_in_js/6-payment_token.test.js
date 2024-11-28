08f073afdbe4ce8b0a4bfe45549fa53d838edc91
Mbaruku2003 <leonardjembe21@gmail.com>
2024-11-28 18:16:54 +0300

    created a new file

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,9 +1,9 @@
-const expect = require('chai');
+const { expect } = require('chai');
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
   it('should return a resolved promise with the correct data when success is true', (done) => {
-    getPaymentTokenAPI(true)
+	  getPaymentTokenFromAPI(true)
       .then((response) => {
         expect(response).to.deep.equal({data: 'Successful response from the API' });
         done();
